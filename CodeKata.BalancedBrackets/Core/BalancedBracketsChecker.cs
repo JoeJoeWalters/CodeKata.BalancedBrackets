73571ff2fdba58a8aa710ff38a115e90a78863ff
joe walters <joewalters999@gmail.com>
2021-10-09 01:41:58 +0100

    Removed stage 1 attempt to avoid confusion

--- CodeKata.BalancedBrackets/Core/BalancedBracketsChecker.cs
+++ CodeKata.BalancedBrackets/Core/BalancedBracketsChecker.cs
@@ -5,22 +5,11 @@ namespace Core
 {
     public class BalancedBracketsChecker
     {
-        private List<char> validBrackets = new List<char>() {'{', '}', '(', ')', '[', ']'};
         private Dictionary<char, char> pairs = new Dictionary<char, char>()
         {
             {'{', '}' },
             {'(', ')' },
             {'[', ']' }
-        };
-
-        private Dictionary<Char, int> bracketBucket = new Dictionary<char, int>()
-        {
-            {'{', 0},
-            {'}', 0},
-            {'(', 0},
-            {')', 0},
-            {'[', 0},
-            {']', 0},
         };
 
         public BalancedBracketsChecker()
@@ -29,20 +18,6 @@ namespace Core
 
         public Boolean Test(string testString)
         {
-            // Stage 1
-            /*
-            foreach (char key in validBrackets)
-            {
-                if (testString.Contains(key))
-                    bracketBucket[key]++;
-            }
-
-            return (bracketBucket['{'] == bracketBucket['}'] &&
-                bracketBucket['('] == bracketBucket[')'] &&
-                bracketBucket['['] == bracketBucket[']']);
-            */
-
-            // Stage 2
             Stack<char> expectedCloses = new Stack<char>();
 
             foreach (char character in testString)
